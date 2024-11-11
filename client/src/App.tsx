46e5e17c067dcc465f9dfabeab26be554f7530ed
Darko Goluža <darkogoluza@gmail.com>
2024-11-11 16:37:40 +0100

    add shadcn to client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,13 @@
+import Lobby from "./views/Lobby";
+
 function App() {
-  return <></>;
+  return (
+    <div className="max-w-screen-2xl mx-auto bg-slate-800 h-screen">
+      <div className="bg-white">
+        <Lobby />
+      </div>
+    </div>
+  );
 }
 
 export default App;
